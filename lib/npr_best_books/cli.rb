e1916a0e5b8c01ea961eb5f26bbdae88142d9df5
beingy <beingy@gmail.com>
2016-06-24 09:11:16 -0700

    update cli interface menu navigation

--- lib/npr_best_books/cli.rb
+++ lib/npr_best_books/cli.rb
@@ -11,10 +11,10 @@ class NPRBestBooks::CLI
   end
 
   def list_books
-    puts "Welcome to NPR's Guide to #{@year}'s Great Reads"
+    puts "+===> Welcome to NPR's Book Concierge <===+"
+    puts "+=====> Guide to #{@year}'s Great Reads <=====+"
     puts <<-DOC.gsub(/^\s*/, "")
-      Latest recommendations in Science Fiction & Fantasy:
-      (in alphabetical order)
+      Science Fiction & Fantasy book recommendations in alphabetical order:
       1. Ancillary Mercy by Ann Leckie
       2. Bitch Planet Vol. 1: Extraordinary Machine by Kelly Sue DeConnick, illustrated by Valentine De Landro
       3. ...
@@ -30,13 +30,22 @@ class NPRBestBooks::CLI
   def menu
     puts "Enter the number of the book you'd want to learn more about:"
     puts "'exit' to end program, 'more' to see recommendations from previous years"
-    print "  Command: "
+    print "+==> Command: "
     input = ""
     while input != "exit"
       input = gets.strip.downcase
       
       if input.to_i >= 1 && input.to_i <= @books.count
-        puts "More info on book #{input}..."
+        puts "+=========================================+"
+        puts "+= " + "Ancillary Mercy by Ann Leckie"
+        puts "+= " + "Recommended by Genevieve Valentine, book critic"
+        puts "+= " + "It would be a mistake to go into Ancillary Mercy hoping for the same lightning-strike of Ancillary Justice. The first installment of Ann Leckie's trilogy opened a world; by now, things are so complicated for protagonist Breq that all you can hope for is to be satisfied. Luckily, Mercy delivers. The world-building particularly shines in the machinations of this closing chapter, as Breq tries to use her new political powers to stop a war with all the weapons the Radchaai Empire has at its disposal: insinuation, shifting loyalties and the second-best tea set."
+        puts "+= " + "ISBN-13: 978-0316246682"
+        puts "+= " + "Paperback: 348 pages"
+        puts "+= " + "Publisher: Orbit (October 5, 2015)"
+        puts "+= " + "Average Amazon Customer Review: 4.3 out of 5 stars (233 reviews)"
+        puts "+= " + "Amazon Buy New Price: $9.59"
+        puts "+=========================================+"
         sub_menu
       elsif input.to_s == "list"
         list_books
@@ -48,15 +57,18 @@ class NPRBestBooks::CLI
           goodbye
       else
           puts "Invalid command, please try again. Error on: '#{input}'."
+          puts "Enter the number of the book you'd want to learn more about:"
+          puts "'exit' to end program, 'more' to see recommendations from previous years"
+          print "+==> Command: "
       end
 
     end
   end
 
   def sub_menu
-    puts "What do you like to do next?"
+    puts "What would you like to do next?"
     puts "'list' to show book list again, 'more' to see recommendations from another year, 'exit' to end program."
-    print "  Command: "
+    print "+==> Command: "
     input = ""
     while input != "exit" || input != "more" || input != "list"
       input = gets.strip.downcase
@@ -68,8 +80,22 @@ class NPRBestBooks::CLI
       elsif input.to_s == "exit" || input.to_s == "bye" || input.to_s == "quit"
         puts "Exiting program..."
         goodbye
+      elsif input.to_i >= 1 && input.to_i <= @books.count
+        puts "+=========================================+"
+        puts "+= " + "Ancillary Mercy by Ann Leckie"
+        puts "+= " + "Recommended by Genevieve Valentine, book critic"
+        puts "+= " + "It would be a mistake to go into Ancillary Mercy hoping for the same lightning-strike of Ancillary Justice. The first installment of Ann Leckie's trilogy opened a world; by now, things are so complicated for protagonist Breq that all you can hope for is to be satisfied. Luckily, Mercy delivers. The world-building particularly shines in the machinations of this closing chapter, as Breq tries to use her new political powers to stop a war with all the weapons the Radchaai Empire has at its disposal: insinuation, shifting loyalties and the second-best tea set."
+        puts "+= " + "ISBN-13: 978-0316246682"
+        puts "+= " + "Paperback: 348 pages"
+        puts "+= " + "Publisher: Orbit (October 5, 2015)"
+        puts "+= " + "Average Amazon Customer Review: 4.3 out of 5 stars (233 reviews)"
+        puts "+= " + "Amazon Buy New Price: $9.59"
+        puts "+=========================================+"
+        sub_menu   
       else
         puts "Invalid command, please try again. Error on: '#{input}'."
+        puts "'list' to show book list again, 'more' to see recommendations from another year, 'exit' to end program."
+        print "+==> Command: "
       end
 
     end
@@ -91,20 +117,30 @@ class NPRBestBooks::CLI
 
   def more_menu
     select_years = []
-    puts "Please select a year to look up more book recommendations:"
+    puts "Please enter the number of the year for more book recommendations:"
     other_years.each.with_index(1) do |year, index|
       puts "#{index}. #{year}"
       select_years << year 
     end
-    print "  Command: "
+    puts "'back' to go back to #{@year}'s book recommendations."
+    print "+==> Command: "
     select_valid_year = false
     while select_valid_year != true
       input = gets.strip
       if select_years.include?(select_years[input.to_i-1])
         change_year(select_years[input.to_i-1])
         select_valid_year = true
+      elsif input == "stay" || input == "back"
+        change_year(@year)
+        select_valid_year = true
       else
         puts "Invalid command. Please try again. Error on '#{input}'."
+        puts "Please enter the number of the year for more book recommendations:"
+        other_years.each.with_index(1) do |year, index|
+          puts "#{index}. #{year}"
+          select_years << year 
+        end
+        print "+==> Command: "
       end
     end
     list_books
@@ -112,7 +148,7 @@ class NPRBestBooks::CLI
   end
 
   def goodbye
-    puts "Goodbye."
+    puts "Thank you for checking out NPR's Book Concierge. Goodbye!"
     exit
   end
 
