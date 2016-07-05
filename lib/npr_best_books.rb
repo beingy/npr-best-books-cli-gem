5bab724596f4f94bd25e60770cbe374cc8015f58
beingy <beingy@gmail.com>
2016-07-05 12:09:38 -0700

    update require specs

--- lib/npr_best_books.rb
+++ lib/npr_best_books.rb
@@ -1,7 +1,8 @@
-require_relative './npr_best_books/version'
+require 'npr_best_books/version'
 
-require_relative './npr_best_books/scraper'
-require_relative './npr_best_books/list'
-require_relative './npr_best_books/book'
-require_relative './npr_best_books/cli'
+require 'npr_best_books/scraper'
+require 'npr_best_books/list'
+require 'npr_best_books/book'
+require 'npr_best_books/cli'
 
+# require_relative './
